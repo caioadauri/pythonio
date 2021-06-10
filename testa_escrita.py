f8656620c6805381397e7fd4f361f4e67c976716
Caio Adauri <caioadaurigomes@gmail.com>
2021-06-10 08:23:41 -0300

    adicao do flush

--- testa_escrita.py
+++ testa_escrita.py
@@ -1,5 +1,13 @@
-arquivo_contatos = open('dados/contatos-escrita.csv', mode='a')
+arquivo_contatos = open('dados/contatos-escrita.csv', mode='w')
 
-contato = '11,Carol,carol@carol.com.br\n'
+contatos = ['11,Carol,carol@carol.com.br\n',
+            '12,Ana,ana@ana.com.br\n',
+            '12,Tais,tais@tais.com.br\n',
+            '14,Felipe,felipe@felipe.com.br\n']
 
-arquivo_contatos.write(contato)
+for contato in contatos:
+    arquivo_contatos.write(contato)
+
+arquivo_contatos.flush()
+
+input('Pressione <Enter> pra encerrar o programa')
